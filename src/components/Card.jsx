0c7739e1606751eefcdb6743d9c8be02018309d9
Hoffmann Axel <axel.hoffmann0205@yahoo.com>
2023-06-17 17:06:05 +0200

    Add tailwind + first component

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react'
+import agentimmo from '../assets/agentimmo.png'
+import couple from '../assets/couple.png'
+
+export default function Card() {
+    const [count, setCount] = useState(0)
+
+  return (
+    <div className='flex flex-col items-center text-white bg-hero-pattern w-full h-96'>
+        
+    </div>
+  )
+}
